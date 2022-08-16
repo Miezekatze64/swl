d4c5864d4416067f9262537a3243e837caefd5f5
Miezekatze64 <miezmoon64@gmail.com>
2022-08-16 22:51:07 +0200

    last test

--- md_to_html.sh
+++ md_to_html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-pandoc -s --to=gfm _README.md --syntax-definition=syntax/swl.xml --metadata title=" " > README.md
+pandoc -s --to=org _README.md --syntax-definition=syntax/swl.xml --metadata title=" " > README.org

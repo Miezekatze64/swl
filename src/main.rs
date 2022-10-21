662a60eba2c6b5faa596daedfd470235ef441857
Miezekatze64 <miezmoon64@gmail.com>
2022-10-21 17:10:52 +0200

    some unimportant changes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 // including used modules
-mod lexer;
-mod util;
-mod parser;
-mod typecheck;
-mod intermediate;
-mod optimizer;
 mod codegen_x86_64;
 mod codegen_x86_64_linux;
 mod codegen_x86_64_windows;
+mod intermediate;
 mod interpreter;
+mod lexer;
+mod optimizer;
+mod parser;
 mod preprocessor;
+mod typecheck;
+mod util;
 
 // including used stdlib function
-use std::{fs, env::args, process::{exit, Command}, collections::HashMap,
-          time::SystemTime};
+use std::{
+    collections::HashMap,
+    env::args,
+    fs,
+    process::{exit, Command},
+    time::SystemTime,
+};
 
 /// Enum representing possible target types
 // TODO(#2): parse target types (-t/--target)
@@ -75,9 +80,9 @@ fn usage(prog_name: String) -> String {
 /// The main function of the compiler
 fn main() {
     let start_time = SystemTime::now();
-    
+
     let mut args_ = args();
-    let arg0 = args_.next().unwrap_or_else(|| {"<program>".into()});
+    let arg0 = args_.next().unwrap_or_else(|| "<program>".into());
 
     let mut error: bool = false;
 
@@ -88,7 +93,7 @@ fn main() {
         Err(e) => {
             eprintln!("ERROR during argument parsing: {e}");
             exit(1);
-        },
+        }
     };
 
     let t = if gnu_args.contains_key("target") {
@@ -111,27 +116,15 @@ fn main() {
 
     // get architecture-specific functions
     type Intrinsics = fn() -> HashMap<&'static str, &'static str>;
-    type Generate   = fn(Vec<intermediate::Inst>, &HashMap<String, usize>,
-                                &Vec<(String, String)>) -> String;
-    let arch_result: Result<(Intrinsics,
-                             Generate), &'static str> =
-        match target {
-            Target::Linux => {
-                Ok((codegen_x86_64::intrinsics, codegen_x86_64_linux::generate))
-            },
-            Target::Bsd => {
-                Err("BSD")
-            },
-            Target::Mac => {
-                Err("MacOS")
-            },
-            Target::Windows => {
-                Ok((codegen_x86_64::intrinsics, codegen_x86_64_windows::generate))
-            },
-            Target::Wasm => {
-                Err("WASM")
-            },
-        };
+    type Generate =
+        fn(Vec<intermediate::Inst>, &HashMap<String, usize>, &Vec<(String, String)>) -> String;
+    let arch_result: Result<(Intrinsics, Generate), &'static str> = match target {
+        Target::Linux => Ok((codegen_x86_64::intrinsics, codegen_x86_64_linux::generate)),
+        Target::Bsd => Err("BSD"),
+        Target::Mac => Err("MacOS"),
+        Target::Windows => Ok((codegen_x86_64::intrinsics, codegen_x86_64_windows::generate)),
+        Target::Wasm => Err("WASM"),
+    };
 
     // check for errors
     let (intrinsics, generate) = match arch_result {
@@ -139,61 +132,84 @@ fn main() {
         Err(e) => {
             eprintln!("Target `{e}` is not implemented (yet)");
             exit(1);
-        },
+        }
     };
-    
 
     // parse arguments
     if gnu_args.contains_key("help") || unix_args.contains_key(&'h') {
         eprintln!("{}", usage(arg0));
         exit(0);
     }
-    
+
     let verbose = if gnu_args.contains_key("verbose") {
         &gnu_args["verbose"]
     } else if unix_args.contains_key(&'v') {
         &unix_args[&'v']
     } else {
         "1"
-    }.parse::<usize>().unwrap_or(0);
+    }
+    .parse::<usize>()
+    .unwrap_or(0);
 
     let interpret = gnu_args.contains_key("interpret") || unix_args.contains_key(&'i');
 
     let filename: String = match pos_args.get(0) {
         Some(a) => a.to_string(),
         None => {
-            eprintln!("{COLOR_RED}ERROR: <filename> not given{COLOR_RESET}\n{}", usage(arg0),
-                      COLOR_RED = color_red(),
-                      COLOR_RESET = color_reset());
+            eprintln!(
+                "{COLOR_RED}ERROR: <filename> not given{COLOR_RESET}\n{}",
+                usage(arg0),
+                COLOR_RED = color_red(),
+                COLOR_RESET = color_reset()
+            );
             exit(1);
-        },
+        }
     };
 
     // get directory of program
-    let path: String = filename.chars().rev().skip_while(|x| x != &'/').collect::<String>().chars().rev().collect();
+    let path: String = filename
+        .chars()
+        .rev()
+        .skip_while(|x| x != &'/')
+        .collect::<String>()
+        .chars()
+        .rev()
+        .collect();
 
     // check for output arg
-    let name: String =  if gnu_args.contains_key("output") {
+    let name: String = if gnu_args.contains_key("output") {
         gnu_args["output"].clone()
     } else if unix_args.contains_key(&'o') {
         unix_args[&'o'].clone()
     } else {
-        filename.chars().rev().skip_while(|x| x != &'.').skip(1).take_while(|x| x != &'/').collect::<String>().chars().rev().collect()
+        filename
+            .chars()
+            .rev()
+            .skip_while(|x| x != &'.')
+            .skip(1)
+            .take_while(|x| x != &'/')
+            .collect::<String>()
+            .chars()
+            .rev()
+            .collect()
     };
 
     if verbose > 2 {
         println!("PATH: {path}, NAME: {name}");
     }
 
-    
-    let contents = fs::read_to_string(filename.clone()).expect("File read error: ").chars().collect();
-//    let (contents, links, links_libs) = preprocessor::preprocess(contents, filename.clone());
-    
+    let contents = fs::read_to_string(filename.clone())
+        .expect("File read error: ")
+        .chars()
+        .collect();
+    //    let (contents, links, links_libs) = preprocessor::preprocess(contents, filename.clone());
+
     // construct parser
-    let mut parser = parser::Parser::new(contents, filename.clone(), verbose, target).unwrap_or_else(|a| {
-        eprintln!("Error reading file: {}", a);
-        exit(1);
-    });
+    let mut parser = parser::Parser::new(contents, filename.clone(), verbose, target)
+        .unwrap_or_else(|a| {
+            eprintln!("Error reading file: {}", a);
+            exit(1);
+        });
 
     if verbose > 0 {
         eprintln!("[*] generating AST");
@@ -204,16 +220,20 @@ fn main() {
     if let Err(ref e) = a {
         for a in e {
             let (t, v) = a;
-            eprintln!("{COLOR_RED}{}: {COLOR_RESET}{}", t, v,
-                      COLOR_RED = color_red(),
-                      COLOR_RESET = color_reset());
+            eprintln!(
+                "{COLOR_RED}{}: {COLOR_RESET}{}",
+                t,
+                v,
+                COLOR_RED = color_red(),
+                COLOR_RESET = color_reset()
+            );
         }
         error = true;
     }
 
     let checked: bool;
-    let mut aliases:   HashMap<String, util::Type> = HashMap::new();
-    let mut globals:   HashMap<String, usize> = HashMap::new();
+    let mut aliases: HashMap<String, util::Type> = HashMap::new();
+    let mut globals: HashMap<String, usize> = HashMap::new();
 
     if let Ok(ref mut ast) = a {
         if verbose > 1 {
@@ -231,10 +251,14 @@ fn main() {
             Err((e, (g, a, ..))) => {
                 for a in e.iter() {
                     let (t, v) = a;
-                    eprintln!("{COLOR_RED}{}: {COLOR_RESET}{}", t, v,
-                              COLOR_RED = color_red(),
-                              COLOR_RESET = color_reset());
-                };
+                    eprintln!(
+                        "{COLOR_RED}{}: {COLOR_RESET}{}",
+                        t,
+                        v,
+                        COLOR_RED = color_red(),
+                        COLOR_RESET = color_reset()
+                    );
+                }
                 if e.into_iter().any(|(lvl, _)| lvl == util::ErrorLevel::Err) {
                     checked = false;
                     error = true;
@@ -244,9 +268,9 @@ fn main() {
                     aliases = a;
                     globals = g;
                 }
-            },
+            }
             Ok((g, a, functions, ..)) => {
-                if ! functions.contains_key(&(None, "main".into())) {
+                if !functions.contains_key(&(None, "main".into())) {
                     eprintln!("error: {}: missing main function", parser.lexer.filename);
                     error = true;
                     checked = false;
@@ -271,12 +295,13 @@ fn main() {
             // optimize AST
             optimizer::optimize(&mut ast, &aliases);
 
-            let (intermediate, globals, externs) = intermediate::gen(ast.1, &mut HashMap::new(), &globals, aliases, 0, 1, true);
+            let (intermediate, globals, externs) =
+                intermediate::gen(ast.1, &mut HashMap::new(), &globals, aliases, 0, 1, true);
             if verbose > 2 {
                 eprintln!("{:#?}", intermediate);
             }
 
-            if ! interpret {
+            if !interpret {
                 // generate assembly
                 if verbose > 0 {
                     eprintln!("[*] generating assembly");
@@ -285,33 +310,40 @@ fn main() {
                 if verbose > 1 {
                     eprintln!("{}", asm);
                 }
-                
+
                 let mut asm_path = path.clone();
                 asm_path.push_str(&name);
                 asm_path.push_str(".asm");
                 match fs::write(asm_path.clone(), asm) {
-                    Ok(_) => {},
+                    Ok(_) => {}
                     Err(a) => {
                         eprintln!("Error writing file: {}", a);
                         exit(1);
-                    },
+                    }
                 };
-                
+
                 // call nasm and ld -> ELF file
                 if verbose > 0 {
                     eprintln!("[*] CMD: nasm -felf64 {asm_path}");
                 }
-                
-                let nasm =  Command::new("nasm").arg("-felf64").arg(asm_path).output().unwrap();
-                if ! nasm.status.success() {
-                    eprintln!("ERROR executing nasm: \n{}", std::str::from_utf8(&nasm.stderr).unwrap());
+
+                let nasm = Command::new("nasm")
+                    .arg("-felf64")
+                    .arg(asm_path)
+                    .output()
+                    .unwrap();
+                if !nasm.status.success() {
+                    eprintln!(
+                        "ERROR executing nasm: \n{}",
+                        std::str::from_utf8(&nasm.stderr).unwrap()
+                    );
                     error = true;
                 }
-                
+
                 let mut obj_path = path.clone();
                 obj_path.push_str(&name);
                 obj_path.push_str(".o");
-                
+
                 let mut outfile = path;
                 outfile.push_str(&name);
                 if target == Target::Windows {
@@ -319,8 +351,8 @@ fn main() {
                 }
 
                 let mut linked_files = parser.links.clone();
-                let linked_libs  = parser.linked_libs.clone();
-                
+                let linked_libs = parser.linked_libs.clone();
+
                 for lib in linked_libs {
                     // search for lib
                     let lib_str = "-l".to_owned() + &lib;
@@ -329,35 +361,62 @@ fn main() {
 
                 let linked_files_str = linked_files.join(" ");
 
-
                 let ld = match target {
                     Target::Linux => {
                         if verbose > 0 {
                             eprintln!("[*] CMD: ld -o {outfile} {obj_path} {linked_files_str} --dynamic-linker /lib/ld-linux-x86-64.so.2");
                         }
 
-                        Command::new("ld").arg("-o").arg(outfile.clone()).arg(obj_path).args(linked_files).arg("--dynamic-linker").arg("/lib/ld-linux-x86-64.so.2").output().unwrap()
-                    },
+                        Command::new("ld")
+                            .arg("-o")
+                            .arg(outfile.clone())
+                            .arg(obj_path)
+                            .args(linked_files)
+                            .arg("--dynamic-linker")
+                            .arg("/lib/ld-linux-x86-64.so.2")
+                            .output()
+                            .unwrap()
+                    }
                     Target::Windows => {
                         let has_mingw = Command::new("x86_64-w64-mingw32-ld").output().is_ok();
-                        let linker = if has_mingw { "x86_64-w64-mingw32-ld" } else { "ld" };
-                            
+                        let linker = if has_mingw {
+                            "x86_64-w64-mingw32-ld"
+                        } else {
+                            "ld"
+                        };
+
                         if verbose > 0 {
                             eprintln!("[*] CMD: {linker} -o {outfile} {obj_path} {linked_files_str} --image-base 0x10000000");
                         }
 
-                        Command::new(linker).arg("-o").arg(outfile.clone()).arg(obj_path).args(linked_files).arg("--dynamic-linker").arg("/lib/ld-linux-x86-64.so.2").arg("--image-base").arg("0x10000000").output().unwrap()
+                        Command::new(linker)
+                            .arg("-o")
+                            .arg(outfile.clone())
+                            .arg(obj_path)
+                            .args(linked_files)
+                            .arg("--dynamic-linker")
+                            .arg("/lib/ld-linux-x86-64.so.2")
+                            .arg("--image-base")
+                            .arg("0x10000000")
+                            .output()
+                            .unwrap()
                     }
-                    _ => unreachable!("should have already exited")
+                    _ => unreachable!("should have already exited"),
                 };
-                
-                if ! ld.status.success() {
-                    eprintln!("ERROR executing ld:\n{}", std::str::from_utf8(&ld.stderr).unwrap());
+
+                if !ld.status.success() {
+                    eprintln!(
+                        "ERROR executing ld:\n{}",
+                        std::str::from_utf8(&ld.stderr).unwrap()
+                    );
                     error = true;
                 }
-                
-                let time = SystemTime::now().duration_since(start_time).expect("not possible: backwards time...").as_secs_f32();
-                
+
+                let time = SystemTime::now()
+                    .duration_since(start_time)
+                    .expect("not possible: backwards time...")
+                    .as_secs_f32();
+
                 eprintln!("[!] compiled {filename} -> {outfile} in {time}s");
             } else {
                 // interpret file
